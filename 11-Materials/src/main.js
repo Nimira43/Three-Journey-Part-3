1ad8b0546a2c95a2342a1464a90963a0c269a504
Nimira43 <n.rabone72@aol.com>
2025-05-09 12:31:25 +0100

    msp: doorColourTexture

--- 11-Materials/src/main.js
+++ 11-Materials/src/main.js
@@ -22,7 +22,9 @@ matcapTexture.colorSpace = THREE.SRGBColorSpace
 
 
 
-const material = new THREE.MeshBasicMaterial()
+const material = new THREE.MeshBasicMaterial({
+  map: doorColourTexture
+})
 
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(0.5, 16, 16),
