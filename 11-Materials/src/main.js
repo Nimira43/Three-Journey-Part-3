0cf4aa4ba027a2810571fab87fc36e174640df02
Nimira43 <n.rabone72@aol.com>
2025-05-09 12:23:52 +0100

    Set up matcap and gradient textures

--- 11-Materials/src/main.js
+++ 11-Materials/src/main.js
@@ -9,11 +9,13 @@ const textureLoader = new THREE.TextureLoader()
 
 const doorColourTexture = textureLoader.load('./textures/door/color.jpg')
 const doorAlphaTexture = textureLoader.load('./textures/door/alpha.jpg')
-const doorAmbientOcclusionTexture = textureLoader.load('./textures/ambientOcclusion/color.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg')
 const doorHeightTexture = textureLoader.load('./textures/door/height.jpg')
 const doorNormalTexture = textureLoader.load('./textures/door/normal.jpg')
 const doorMetalnessTexture = textureLoader.load('./textures/door/metalness.jpg')
 const doorRoughnessTexture = textureLoader.load('./textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('./textures/matcaps/1.png')
+const gradientTexture = textureLoader.load('./textures/gradients/3.jpg')
 
 const material = new THREE.MeshBasicMaterial()
 
