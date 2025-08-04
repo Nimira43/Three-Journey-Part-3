c70afddd21a97c73d9e06de871c4c8eb8955c0d1
Nimira43 <n.rabone72@aol.com>
2025-08-04 07:03:59 +0100

    Importing lil-gui

--- 11-Materials/src/main.js
+++ 11-Materials/src/main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import './main.css'
+import GUI from 'lil-gui'
 
 const canvas = document.querySelector('.canvas')
 const scene = new THREE.Scene()
