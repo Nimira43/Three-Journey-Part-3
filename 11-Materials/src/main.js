4e63ed4d959214c3ff3a135df883681d27404575
Nimira43 <n.rabone72@aol.com>
2025-08-04 07:55:20 +0100

    aoMapIntensity

--- 11-Materials/src/main.js
+++ 11-Materials/src/main.js
@@ -33,9 +33,11 @@ material.metalness = 0.7
 material.roughness = 0.2
 material.map = doorColourTexture
 material.aoMap = doorAmbientOcclusionTexture
+material.aoMapIntensity = 3
 
 gui.add(material, 'metalness').min(0).max(1).step(0.0001)
 gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+gui.add(material, 'aoMapIntensity').min(0).max(20).step(1)
 
 
 const sphere = new THREE.Mesh(
